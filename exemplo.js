c11e231bfa0c146b2adb14d6de78f2bf8a2c6338
xxrenan <37024152+xxrenan@users.noreply.github.com>
2018-06-16 10:01:55 -0300

    Create exemplo.js

---
+++ exemplo.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var session = require('express-session');
+var app = express();
+
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+app.get('/', function (req, res) {
+	  if (req.session.count) {
+		req.session.count++
+		res.setHeader('Content-Type', 'text/html')
+		res.write('<p>Contador: ' + req.session.count + '</p>')
+		res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+		res.end()
+	  } else {
+		req.session.count = 1
+		res.end('welcome to the session demo. refresh!')
+	  }
+});
+
+app.post('/login', upload.array(), function (req, res, next) {
+  console.log(req.body);
+  if (req.session.auth==true) {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><meta charset="utf-8"></head><body><p>Você já está logado</p></body></html>');
+    res.end();
+  } else if(req.body.nome=="maicon" && req.body.senha=="teste") {
+		req.session.auth= true;
+    	res.end('welcome to the session demo. refresh!');
+  } else {
+		res.end('try again');
+  }
+});
+
+app.get('/count', function (req, res) {
+  if (req.session.count) {
+    req.session.count++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>Contador: ' + req.session.count + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    res.end('no session demo, go to root!')
+  }
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+//req.session.destroy();
